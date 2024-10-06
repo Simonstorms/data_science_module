35ef8ac362687f7afee327e3e066e60d6ef0109d
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-10-07 00:24:06 +0200

    till task 6

--- last_letter/main.py
+++ last_letter/main.py
@@ -1,30 +1,59 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv('yob/yob2021.txt', names=['Name', 'Gender', 'Quantity'], header=None)
+### Load the data from text files into dataframes
+dataframes = [pd.read_csv(f'yob/yob{year}.txt', names=["Name", "Gender", "Count"]).assign(Year=year) for year in range(1880, 2015)]
 
-# print("First ten rows: \n ", df.head(10))
-# print("Number of rows and columns: \n ", df.shape)
+### Combine all dataframes into a single dataframe
+all_data = pd.concat(dataframes, ignore_index=True)
 
-# print("total number of babies: \n ", df["Name"].count())
+### Create a new column with the last letter of each name
+all_data['LastLetter'] = all_data['Name'].str[-1]
 
-# print("Total number of male babies:", (df["Gender"] == "M").sum(),
-# "\nTotal number of female babies:", (df["Gender"] == "F").sum())
 
-# print("Is my Name 'Simon' in the data:", (df["Name"] == "Simon").any())
 
-# print(f"Percentage of Women: {((df["Gender"] == "F").sum() / df["Name"].count()) * 100:.2f}%",
+### Plot the bar chart of last letters of names
+# last_letter = all_data.groupby('LastLetter')['Count'].sum()
+# last_letter.plot.bar()
+# plt.title('Total Count of Names by Last Letter')
+# plt.xlabel('Last Letter of Names')
+# plt.ylabel('Total Count')
+# plt.show()
 
-# "Percentage of Man: {((df["Gender"] == "M").sum() / df["Name"].count()) * 100:.2f}%")
 
-# print("top 5 girls names and top 5 boys names: \n", pd.concat([
-#     df[df["Gender"] == "F"].head(5)["Name"],
-#     df[df["Gender"] == "M"].head(5)["Name"]
-# ]).reset_index(drop=True)
-# )
 
-# pd.concat([
-#     df[df["Gender"] == "F"].head(5)["Name"],
-#     df[df["Gender"] == "M"].head(5)["Name"]
-# ]).reset_index(drop=True).to_excel("yob2021_excel.xlsx")
+### count for specific letter "s" in the years
+# filtered_data = all_data[all_data['LastLetter'] == "s"]
+# timeline_data = filtered_data.groupby('Year')['Count'].sum().reset_index()
+# timeline_data.columns = ['Year', 'Count']
+# print(timeline_data)
+#
+# timeline_data.plot(x='Year', y='Count')
+# plt.show()
 
+
+
+### multiple timelines for letters
+# grouped_data = all_data.groupby(['LastLetter', 'Year'])['Count'].sum()
+# crosstable = grouped_data.unstack(0)
+#
+# letters_to_plot = ['s', 'r', 'p']
+# for letter in letters_to_plot:
+#     if letter in crosstable.columns:
+#         plt.plot(crosstable.index, crosstable[letter])
+#
+# plt.show()
+
+
+
+### Heatmap of first and last letters
+all_data['Name'] = all_data['Name'].str.upper()
+all_data['FirstLetter'] = all_data['Name'].str[0]
+all_data['LastLetter'] = all_data['Name'].str[-1]
+crosstab = pd.crosstab(all_data['FirstLetter'], all_data['LastLetter'], values=all_data['Count'], aggfunc='sum')
+crosstab = crosstab.sort_index(axis=0)
+
+print(crosstab)
+sns.heatmap(crosstab)
+plt.show()
