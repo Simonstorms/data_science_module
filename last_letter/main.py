ac27abcf3195f64097396133534313dc3259e3e6
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-10-06 23:14:11 +0200

    add new homework challange

---
+++ last_letter/main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('yob/yob2021.txt', names=['Name', 'Gender', 'Quantity'], header=None)
+
+# print("First ten rows: \n ", df.head(10))
+# print("Number of rows and columns: \n ", df.shape)
+
+# print("total number of babies: \n ", df["Name"].count())
+
+# print("Total number of male babies:", (df["Gender"] == "M").sum(),
+# "\nTotal number of female babies:", (df["Gender"] == "F").sum())
+
+# print("Is my Name 'Simon' in the data:", (df["Name"] == "Simon").any())
+
+# print(f"Percentage of Women: {((df["Gender"] == "F").sum() / df["Name"].count()) * 100:.2f}%",
+
+# "Percentage of Man: {((df["Gender"] == "M").sum() / df["Name"].count()) * 100:.2f}%")
+
+# print("top 5 girls names and top 5 boys names: \n", pd.concat([
+#     df[df["Gender"] == "F"].head(5)["Name"],
+#     df[df["Gender"] == "M"].head(5)["Name"]
+# ]).reset_index(drop=True)
+# )
+
+# pd.concat([
+#     df[df["Gender"] == "F"].head(5)["Name"],
+#     df[df["Gender"] == "M"].head(5)["Name"]
+# ]).reset_index(drop=True).to_excel("yob2021_excel.xlsx")
+
--- test_project_fo _lu/main.py
+++ test_project_fo _lu/main.py
@@ -1,14 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
-df2 = pd.read_csv('admission_data.csv')
-
-# df2 = df[df["Major"] != "Other"].copy()
-df2.loc[:, "acc"] = df2["Admission"].replace({"Accepted": "1", "Rejected": "0"}).astype(int)
+import seaborn as sns
+df = pd.read_csv('countries.csv')
 
 
-# print(df2.groupby("Major")["acc"].mean()*100)
-s = df2.groupby(["Major","Gender"])["acc"].mean().unstack().plot.bar()
-print(s)
+
+sns.scatterplot(x='area (sq.km)', y='population', c=df['pop_density']/5, data=df)
 plt.show()
-
